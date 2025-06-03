1666983fd67f8d06b00840ee4a533e6cb1fd9532
Petra Gianni Sanhueza Maffezzoli <142603915+P-Maffezzoli@users.noreply.github.com>
2025-06-02 21:01:17 -0300

    Add files via upload
    primeiro projeto de teste feito no VSCommunity

---
+++ ProjetoTeste/ProjetoTeste/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ProjetoTeste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            int idade = 20;
+            string nome = null;
+            double nota = 8.5;
+            char letra = 'B';
+            bool aprovado = true;
+
+            //Console.WriteLine(PI);
+            //Console.WriteLine(idade);
+            Console.WriteLine("Qual seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine(nome);
+            //Console.WriteLine(nota);
+            //Console.WriteLine(letra);
+            //Console.WriteLine(aprovado);
+            //Console.WriteLine($"seu nome é {nome},tens {idade} anos e sua nota foi {nota} com avaliação {letra}");
+
+
+       }
+    }
+}
