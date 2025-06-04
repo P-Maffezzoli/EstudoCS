58f6961525abc6785449b9a81428590241b9b599
Petra Gianni Sanhueza Maffezzoli <142603915+P-Maffezzoli@users.noreply.github.com>
2025-06-03 21:38:12 -0300

    Add files via upload
    começando exercícios de For e While

---
+++ ExerciciosForWhile/ExerciciosForWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿//Soma de 10 Números - Peça para o usuário digitar 10 números.
+//Use for ou while para somar todos eles e mostrar o resultado.​
+using System;
+
+namespace ExerciciosForWhile1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int soma = 0;
+            Console.WriteLine("informe 10 números para soma-los:");
+
+            for (int i = 0; i < 10 i++);
+            {
+                int n = int.Parse(Console.ReadLine());
+                int numeros = n;
+            }
+            //foreach (int i in numeros)
+            //{
+            // soma = soma + numeros[i++];
+            //}
+          Console.WriteLine(numeros);
+        }
+    }
+}
